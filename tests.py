2d19f417af277f9da09746186a4a351968beef41
kainpets <pawelstepniak22@gmail.com>
2020-06-03 21:05:52 +0200

    Complete another exercises

--- tests.py
+++ tests.py
@@ -1,5 +1,20 @@
-print("start")
-for i in range(0):
-    print("Hello")
+from graphics import *
 
-print("end")
+win = GraphWin('Shapes')
+
+center = Point(100,100)
+circ = Circle(center, 30)
+circ.setFill('red')
+circ.draw(win)
+
+label = Text(center, "Red Circle")
+label.draw(win)
+
+rect = Rectangle(Point(30,30), Point(70,70))
+rect.draw(win)
+
+line = Line(Point(20,30), Point(180,165))
+line.draw(win)
+
+oval = Oval(Point(20,150), Point(180,199))
+oval.draw(win)
