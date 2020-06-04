b58ccd6df1aaa6c79dac314f01c3490698615211
kainpets <pawelstepniak22@gmail.com>
2020-06-04 11:00:51 +0200

    Complete first exercise from chapter 4

---
+++ 4.1.py
@@ -0,0 +1,16 @@
+from graphics import *
+
+def main():
+    win = GraphWin()
+    shape = Rectangle(Point(50,50), Point(20,20))
+    shape.setOutline("red")
+    shape.setFill("red")
+    shape.draw(win)
+    for i in range(10):
+        p = win.getMouse()
+        c = shape.getCenter()
+        dx = p.getX() - c.getX()
+        dy = p.getY() - c.getY()
+        shape.move(dx, dy)
+    win.close()
+main()
