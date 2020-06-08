89f21b1818dc80dd89924ed4450c4dbe3d2ffd46
kainpets <pawelstepniak22@gmail.com>
2020-06-08 15:46:02 +0200

    Finish ex 4.3, begin 4.4

---
+++ 4.4.py
@@ -0,0 +1,15 @@
+from graphics import *
+
+def main():
+    # set the graphics window
+    win = GraphWin()
+    win.setCoords(-6,-6, 6,6)
+
+    # draw the tree stump
+    
+    
+    # close after mouse click
+    win.getMouse()
+    win.close()
+    
+main()
