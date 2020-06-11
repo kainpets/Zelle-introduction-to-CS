d2f873e593bfc4ecc65467e9e3a2a78896661aaf
kainpets <pawelstepniak22@gmail.com>
2020-06-12 01:00:32 +0200

    Finish 4.9

---
+++ 4.9.py
@@ -0,0 +1,30 @@
+from graphics import *
+from math import *
+
+def main():    
+    # draw the graphics window
+    win = GraphWin("window", 600,600)
+    win.setCoords(-10,-10,10,10)
+
+    # gather mouse clicks from the user
+    corner1 = win.getMouse()
+    corner2 = win.getMouse()
+
+    # draw the rectangle
+    rec = Rectangle(corner1, corner2)
+    rec.draw(win)
+
+    # close the program after the last click
+    win.getMouse()
+    win.close()
+
+    # calculate the perimeter and area of the rectangle
+    length = abs(corner1.getX() - corner2.getX())
+    width = abs(corner1.getY() - corner2.getY())
+    area = length * width
+    perimeter = 2 * (length + width)
+
+    # print the x value of the points of intersection
+    print("The area is: ",area,"The perimeter is: ", perimeter)
+
+main()
