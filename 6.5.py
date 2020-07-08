9fffd8c45bcbd063ac5734863059daea9aab01a8
kainpets <pawelstepniak22@gmail.com>
2020-07-08 23:36:09 +0200

    Finished exercise 6.5

---
+++ 6.5.py
@@ -0,0 +1,16 @@
+import math
+
+diameter = float(input("Enter the diameter of your pizza: "))
+price = float(input("Enter the price of your pizza: "))
+
+def areaOfPizza(diameter):
+    return math.pi * (diameter/2)**2
+
+def costSquareInch(diameter, price):
+    return price / areaOfPizza(diameter)
+
+def main():
+    print("The area of the pizza is %0.4f" % (areaOfPizza(diameter)))
+    print("The cost of square unit of pizza is %0.4f" % (costSquareInch(diameter, price)))
+    
+main()
