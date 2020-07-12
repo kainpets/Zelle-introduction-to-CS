a84fc353fc63e6a8bc1ced27bfee75342705f095
kainpets <pawelstepniak22@gmail.com>
2020-07-12 23:34:40 +0200

    Finished ex 6.8

--- 6.8.py
+++ 6.8.py
@@ -6,14 +6,17 @@ print("This program estimates the square root of a number")
 
 def nextGuess(guess, x):
     guess = (guess + (x/guess))/2
+    return guess
+
+def improve(x, chances):
+    guess = x / 2
+    for i in range(chances):
+        guess = nextGuess(guess, x)
+    return guess
 
 def main():
-    x = float(input("Enter a number: "))
-    guess = x/2
-    answer = math.sqrt(x)
-    print("My guess is: ", nextGuess(x))
-    print("The answer is: % 0.4f" % (answer))
-    #print("I was this close to guessing correctly: %0.4f" % (finalAnswer))
-
-
+    x = float(input("Enter the number to take the root of: "))
+    chances = int(input("Enter the number of times I can improve my guess: "))
+    print("The square root is: ", improve(x, chances))
+    
 main()
--- 6.9.py
+++ 6.9.py
@@ -1,13 +1,14 @@
 # Program the tells the user which grade he recieved based on their
 #   test result using functions
-result = 0
 
-def grade(score):
+
+def grade(score, result):
     result = "FFFFFFDCBAA"[score//10]
     return result
 
 def main():
     score = int(input("Your score: "))
+    result = 0
     print("Your grade: ", grade(score))
 
 main()
